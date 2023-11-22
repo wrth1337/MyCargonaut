3bac8959cb16ddf5eeb38234e4674b4187ccd7a5
marcel <marcel.philipp.kaiser@mni.thm.de>
2023-11-22 15:51:50 +0100

    initial angular + database setup

--- public/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  var challengeElement = document.getElementById("js-challenge");
-  if (challengeElement) {
-    challengeElement.innerHTML = "working";
-    challengeElement.classList.remove("text-red");
-    challengeElement.classList.add("text-green");
-  }
-});
