14417427af48341f5089d1635b579945ecb1083f
Benjamin Wirth <benjamin.wirth@mni.thm.de>
2023-12-04 16:05:25 +0100

    Add Karma-Tests for register component.

--- frontend/mycargonaut/src/app/auth/components/register/register.component.spec.ts
+++ frontend/mycargonaut/src/app/auth/components/register/register.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RegisterComponent } from './register.component';
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {By} from "@angular/platform-browser";
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
@@ -20,5 +21,15 @@ describe('RegisterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a username input field with type=text', () =>{
+    const el = fixture.debugElement.query(By.css('.username-input'));
+    expect(el.nativeElement.getAttribute('type')).toEqual('text');
+  });
+
+  it('should have a password input field with type=password', () =>{
+    const el = fixture.debugElement.query(By.css('.password-input'));
+    expect(el.nativeElement.getAttribute('type')).toEqual('password');
   });
 });
