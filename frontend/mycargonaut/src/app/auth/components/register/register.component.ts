a66f877ba21fec4f8362c80b8f8f04fec5a10f5d
Jonathan Rech <Jonathan.wolfgang.hinton.rech@mni.thm.de>
2023-12-04 12:44:52 +0100

    Add API Service, Update register component sends the form value to the backend for user/register

--- frontend/mycargonaut/src/app/auth/components/register/register.component.ts
+++ frontend/mycargonaut/src/app/auth/components/register/register.component.ts
@@ -1,15 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+  isLogin: boolean = false;
 
+  constructor(
+    private api: ApiService,
+  ){}
   onSubmit(form: NgForm){
     console.log("Form triggered")
+    this.api.postRequest("user/register", form.value)
+  }
+  ngOnInit(): void {
+      console.log("init functionality")
   }
 }
