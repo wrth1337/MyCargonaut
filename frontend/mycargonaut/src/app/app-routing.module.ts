3d3f30422b1a4941947bfd17762ddc8e7f58ecab
Ella Herr <ella.herr@mni.thm.de>
2023-12-11 16:04:48 +0100

    Main driectory für Components added, landingpage commit

--- frontend/mycargonaut/src/app/app-routing.module.ts
+++ frontend/mycargonaut/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './auth/components/register/register.component';
+import {HomeComponent} from "./main/home/home.component";
 
 const routes: Routes = [
   {path: 'register', component: RegisterComponent},
+  {path: '', component: HomeComponent},
 ];
 
 @NgModule({
--- frontend/mycargonaut/src/app/app.module.ts
+++ frontend/mycargonaut/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AuthModule} from "./auth/auth.module";
+import { HomeComponent } from './main/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     AuthModule,
