7622556ad7134df17437740d261ca4e284841a03
Benjamin Wirth <benjamin.wirth@mni.thm.de>
2023-12-15 11:33:54 +0100

    Add frontend-tests for register; Fix test-bug

--- frontend/mycargonaut/src/app/auth/components/register/register.component.spec.ts
+++ frontend/mycargonaut/src/app/auth/components/register/register.component.spec.ts
@@ -23,13 +23,38 @@ describe('RegisterComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have a username input field with type=text', () =>{
-    const el = fixture.debugElement.query(By.css('.username-input'));
+  it('should have a firstName input field with type=text', () =>{
+    const el = fixture.debugElement.query(By.css('.firstName-input'));
+    expect(el.nativeElement.getAttribute('type')).toEqual('text');
+  });
+
+  it('should have a lastName input field with type=text', () =>{
+    const el = fixture.debugElement.query(By.css('.lastName-input'));
+    expect(el.nativeElement.getAttribute('type')).toEqual('text');
+  });
+
+  it('should have a birthdate input field with type=date', () =>{
+    const el = fixture.debugElement.query(By.css('.birthdate-input'));
+    expect(el.nativeElement.getAttribute('type')).toEqual('date');
+  });
+
+  it('should have a phonenumber input field with type=tel', () =>{
+    const el = fixture.debugElement.query(By.css('.phonenumber-input'));
+    expect(el.nativeElement.getAttribute('type')).toEqual('tel');
+  });
+
+  it('should have a email input field with type=text', () =>{
+    const el = fixture.debugElement.query(By.css('.email-input'));
     expect(el.nativeElement.getAttribute('type')).toEqual('text');
   });
 
   it('should have a password input field with type=password', () =>{
     const el = fixture.debugElement.query(By.css('.password-input'));
+    expect(el.nativeElement.getAttribute('type')).toEqual('password');
+  });
+
+  it('should have a password input field for a repeating password with type=password', () =>{
+    const el = fixture.debugElement.query(By.css('.passwordRepeat-input'));
     expect(el.nativeElement.getAttribute('type')).toEqual('password');
   });
 });
