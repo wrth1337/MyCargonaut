da573302963c66007f934a944d9bbc597152ea78
Jonathan Rech <Jonathan.wolfgang.hinton.rech@mni.thm.de>
2023-12-17 12:05:43 +0100

    Fix: Test

--- frontend/mycargonaut/src/app/app.component.spec.ts
+++ frontend/mycargonaut/src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
+    imports: [RouterTestingModule, AuthModule],
     declarations: [AppComponent]
   }));
 
