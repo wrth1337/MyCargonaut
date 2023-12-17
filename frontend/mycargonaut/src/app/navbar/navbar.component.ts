e287fbe3b10b39c13e7faabeab99199be5534ecc
Marcel Kaiser <marcel.philipp.kaiser@mni.thm.de>
2023-12-17 12:43:30 +0100

    fix: frontend tests

--- frontend/mycargonaut/src/app/navbar/navbar.component.ts
+++ frontend/mycargonaut/src/app/navbar/navbar.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
