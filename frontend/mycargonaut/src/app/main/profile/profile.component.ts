74062753e028566899caa25809a21362acb3c003
Nico Binder <nicolas.daniel.binder@mni.thm.de>
2023-12-19 18:13:44 +0100

    format dates correctly

--- frontend/mycargonaut/src/app/main/profile/profile.component.ts
+++ frontend/mycargonaut/src/app/main/profile/profile.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
+import { DatePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
+  providers: [DatePipe]
 })
 
 
@@ -23,13 +25,15 @@ export class ProfileComponent {
   tripsAvailable = false;
 
   constructor(
-    private api: ApiService
+    private api: ApiService,
+    private datePipe: DatePipe
   ){}
 
   ngOnInit() {
     console.log("init");
     this.api.getUserProfile().subscribe((res: any) => {
       this.userData = res.userData;
+      this.userData.birthdate = this.datePipe.transform(res.userData.birthdate, 'dd.MM.yyyy');
     });
     this.api.getUserVehicles().subscribe((res: any) => {
       if(res.status === 1) {
@@ -44,6 +48,9 @@ export class ProfileComponent {
       if(res.status === 1) {
         this.offersAvailable = true;
         this.offerData = res.offerData;
+        for(let i = 0; i < this.offerData.length; i++) {
+          this.offerData[i].startDate = this.datePipe.transform(res.offerData[i].startDate, 'dd.MM.yyyy');
+        }
       }
       else {
         this.offerData = res.message;
@@ -53,6 +60,9 @@ export class ProfileComponent {
       if(res.status === 1) {
         this.wantedsAvailable = true;
         this.wantedData = res.wantedData;
+        for(let i = 0; i < this.wantedData.length; i++) {
+          this.wantedData[i].startDate = this.datePipe.transform(res.wantedData[i].startDate, 'dd.MM.yyyy');
+        }
       }
       else {
         this.wantedData = res.message;
@@ -63,6 +73,12 @@ export class ProfileComponent {
         this.tripsAvailable = true;
         this.uwtData = res.uwtData;
         this.uotData = res.uotData;
+        for(let i = 0; i < this.uwtData.length; i++) {
+          this.uwtData[i].startDate = this.datePipe.transform(res.uwtData[i].startDate, 'dd.MM.yyyy');
+        }
+        for(let i = 0; i < this.uotData.length; i++) {
+          this.uotData[i].startDate = this.datePipe.transform(res.uotData[i].startDate, 'dd.MM.yyyy');
+        }
       }
       else {
         this.uwtData = res.message;
