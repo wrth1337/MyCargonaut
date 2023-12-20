30904bc670970eedafd19a80f660e247dc776d4c
hannafeddersen <hanna.feddersen@mni.thm.de>
2023-12-20 17:04:55 +0100

    Add Angular Frontend Component searchbar

---
+++ frontend/mycargonaut/src/app/searchbar/searchbar.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ApiService } from 'src/app/service/api.service';
+
+@Component({
+  selector: 'app-searchbar',
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.css']
+})
+export class SearchbarComponent {
+  constructor(private api: ApiService) {}
+  onSubmit(form: NgForm) {
+    console.log("Form triggered");
+    console.log(form.value);
+
+    const queryParams = Object.keys(form.value)
+      .map(key => {
+        const value = form.value[key];
+        const encodedValue = encodeURIComponent(value !== undefined ? value : 'null');
+        return `${encodeURIComponent(key)}=${encodedValue}`;
+      })
+      .join('&');
+    const url = `searchbar/search?${queryParams}`;
+
+    this.api.getRequest(url).subscribe((res: any) => {
+      if(res == null){ console
+        .log("res leer");
+      }else {
+        console.log(res);
+      }
+    });
+  }
+}
