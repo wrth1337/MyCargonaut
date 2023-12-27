6c49164136f3024612ce4a4f579c3ab9c1428eb3
Jonathan Rech <Jonathan.wolfgang.hinton.rech@mni.thm.de>
2023-12-27 16:10:43 +0100

    Update frontend to be inline with mockup update: HTML / CSS add: carousel implementation (without animation) WIP

--- frontend/mycargonaut/src/app/main/home/home.component.ts
+++ frontend/mycargonaut/src/app/main/home/home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+   
+export class HomeComponent implements OnInit{
+  public numbers = [1,2,3,4,5,6,7]
+  number = 1
+  index = 2;
+  public content = [{Title: 'Suche Transport von Gießen nach Marburg 1', Text: 'Ich Suche einen Transport meines Klaviers von Gießen nach Marburg bis ende des Jahres. Wenn Sie einen Kleintransporter oder ähnliches haben und auf dem Weg von Gießen nach Marburg sind, würde ich mich über ein Angebot freuen. 1'},{Title: 'Suche Transport von Gießen nach Marburg 2', Text: 'Ich Suche einen Transport meines Klaviers von Gießen nach Marburg bis ende des Jahres. Wenn Sie einen Kleintransporter oder ähnliches haben und auf dem Weg von Gießen nach Marburg sind, würde ich mich über ein Angebot freuen. 2'},{Title: 'Suche Transport von Gießen nach Marburg 3', Text: 'Ich Suche einen Transport meines Klaviers von Gießen nach Marburg bis ende des Jahres. Wenn Sie einen Kleintransporter oder ähnliches haben und auf dem Weg von Gießen nach Marburg sind, würde ich mich über ein Angebot freuen. 3'},{Title: 'Suche Transport von Gießen nach Marburg 4', Text: 'Ich Suche einen Transport meines Klaviers von Gießen nach Marburg bis ende des Jahres. Wenn Sie einen Kleintransporter oder ähnliches haben und auf dem Weg von Gießen nach Marburg sind, würde ich mich über ein Angebot freuen. 4'},{Title: 'Suche Transport von Gießen nach Marburg 5', Text: 'Ich Suche einen Transport meines Klaviers von Gießen nach Marburg bis ende des Jahres. Wenn Sie einen Kleintransporter oder ähnliches haben und auf dem Weg von Gießen nach Marburg sind, würde ich mich über ein Angebot freuen. 5'},{Title: 'Suche Transport von Gießen nach Marburg 6', Text: 'Ich Suche einen Transport meines Klaviers von Gießen nach Marburg bis ende des Jahres. Wenn Sie einen Kleintransporter oder ähnliches haben und auf dem Weg von Gießen nach Marburg sind, würde ich mich über ein Angebot freuen. 6'}];
+  public activeContent = this.content.slice(0,3);
+  
+  constructor(
+    private api: ApiService
+  ){}
+  
+  ngOnInit(): void {
+    console.log('init');
+    this.api.getRequest('offer/last').subscribe((res:any) => {
+      this.content = res.content;
+    })
+  }
+  next() {
+    console.log('next');
+    
+    this.index++;
+    this.index = this.index % 6;
+    this.activeContent[0] = this.activeContent[1];
+    this.activeContent[1] = this.activeContent[2];
+    this.activeContent[2] = this.content[this.index]
 
+  }
 }
