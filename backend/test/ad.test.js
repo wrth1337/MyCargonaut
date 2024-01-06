b0f9cf39a8734145f5844d699bba50e0187f7459
Jonathan Rech <Jonathan.wolfgang.hinton.rech@mni.thm.de>
2024-01-06 20:09:18 +0100

    fix test + ad picture attribute

--- backend/test/ad.test.js
+++ backend/test/ad.test.js
@@ -17,10 +17,10 @@ test('get ad by correct Id', async () =>{
                 VALUES (10,'Max', 'Mustermann', 'max@example.com', 'pass123', '1990-05-15', '123456789', 100.0, 'user1.jpg', 'Hi was geht so', 'Viel Erfahrung')`);
         await conn.query(`INSERT INTO ad (adId,description, startLocation, endLocation, startDate, endDate, animals, smoker, notes, numSeats, userId) \
                 VALUES (10,'Ja Beschreibung halt so lololol', 'City A', 'City B', '2023-01-10', '2023-01-15', 0, 1, 'No pets allowed', 4, 10)`);
-        await conn.query(`INSERT INTO vehicle (name, numSeats, maxWeight, picture, loadingAreaDimensions, specialFeautures, userId) \
-                VALUES ('Car1', 4, 500.0, 'car1.jpg', '2x2x2', 'GPS, Bluetooth', 10)`);
+        await conn.query(`INSERT INTO vehicle (vehicleId, name, numSeats, maxWeight, picture, loadingAreaDimensions, specialFeautures, userId) \
+                VALUES (10,'Car1', 4, 500.0, 'car1.jpg', '2x2x2', 'GPS, Bluetooth', 10)`);
         await conn.query(`INSERT INTO offer (offerId, vehicleId, adId, pricePerPerson, pricePerFreight) \
-                VALUES (10, 1, 10, 50.0, 100.0)`);
+                VALUES (10, 10, 10, 50.0, 100.0)`);
         const res = (await ad.getAdById(10));
         expect(res.data.result[0]).toEqual(
             {
@@ -36,12 +36,14 @@ test('get ad by correct Id', async () =>{
                 'smoker': 1,
                 'notes': 'No pets allowed',
                 'numSeats': 4,
+                'picture': null,
                 'active': 1,
                 'userId': 10,
             });
     } finally {
         conn.query(`DELETE FROM ad WHERE adId = 10`);
         conn.query(`DELETE FROM offer WHERE offerId = 10`);
+        conn.query(`DELETE FROM vehicle WHERE vehicleId = 10`);
         conn.query(`DELETE FROM user WHERE userId = 10`);
         if (conn) await conn.release();
     };
