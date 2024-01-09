2a2da90b071a3bb5caae8668324e33b38199f8a5
Nico Binder <nicolas.daniel.binder@mni.thm.de>
2024-01-09 15:24:57 +0100

    edit buttons add feedback when submit button is pressed redirect user to edit profile when clicking edit button add feedback when confirm button is clicked (but has no real function)

--- frontend/mycargonaut/src/app/main/wanted/wanted.component.ts
+++ frontend/mycargonaut/src/app/main/wanted/wanted.component.ts
@@ -22,6 +22,9 @@ export class WantedComponent {
   pet: boolean = true;
   stars: number[] = [1, 2, 3, 4, 5];
   tripCount: any;
+  showFlashMessage: boolean = false;
+  success: boolean = false;
+  approved: boolean = false;
 
   ngOnInit() {
     this.api.getRequest("profile/userdata").subscribe((res: any) => {
@@ -46,7 +49,11 @@ export class WantedComponent {
     form.value.notes = null;
 
     this.api.postRequest("wanted/createWanted", form.value).subscribe((res: any) => {
+      if(res.status === 1) {
+        this.success = true;
+      }
     });
+    this.showFlash();
   }
 
   updateSmoke() {
@@ -55,5 +62,21 @@ export class WantedComponent {
 
   updatePet() {
     this.pet = !this.pet;
+  }
+
+  showFlash() {
+    this.showFlashMessage = true;
+
+    setTimeout(() => {
+      this.closeFlashMessage();
+    }, 5000);
+  }
+
+  closeFlashMessage() {
+    this.showFlashMessage = false;
+  }
+
+  approveUserdata() {
+    this.approved = true;
   }
 }
