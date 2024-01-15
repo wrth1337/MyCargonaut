7630771772e7a3b83c85341a12cbe917f22c0e93
Ella Herr <ella.herr@mni.thm.de>
2024-01-15 10:42:50 +0100

    Imports

--- frontend/mycargonaut/src/app/main/create-offer/create-offer.component.spec.ts
+++ frontend/mycargonaut/src/app/main/create-offer/create-offer.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateOfferComponent } from './create-offer.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DatePipe } from '@angular/common';
+import { NgForm } from '@angular/forms';
 import { ApiService} from "src/app/service/api.service";
 import { FormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
