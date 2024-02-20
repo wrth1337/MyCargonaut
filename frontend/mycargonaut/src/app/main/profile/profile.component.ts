92a13696e568aefee04ad8364b4eca4f0350278a
Nico Binder <nicolas.daniel.binder@mni.thm.de>
2024-02-20 18:45:42 +0100

    add ngOnInit

--- frontend/mycargonaut/src/app/main/profile/profile.component.ts
+++ frontend/mycargonaut/src/app/main/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
 import { DatePipe } from '@angular/common';
 import { AuthService } from 'src/app/service/auth.service';
@@ -12,7 +12,7 @@ import { AuthService } from 'src/app/service/auth.service';
 })
 
 
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
   userData: any;
   vehicleData: any;
   offerData: any;
@@ -48,7 +48,7 @@ export class ProfileComponent {
     private datePipe: DatePipe
   ){}
 
-  OnInit() {
+  ngOnInit() {
     const userId = JSON.parse(this.auth.getUserData() || '{user_id = 0}').user_id;
     this.api.getRequest("profile/userdata/"+userId).subscribe((res: any) => {
       this.userData = res.userData;
