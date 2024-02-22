ee03ead7adae9c6635a415179b8289d94f71d3a0
marcel <marcel.philipp.kaiser@mni.thm.de>
2024-02-22 18:59:55 +0100

    fix await race condition

--- backend/test/coin.test.js
+++ backend/test/coin.test.js
@@ -1,4 +1,4 @@
-const {expect, test, afterAll, beforeAll} = require('@jest/globals');
+const {expect, test, afterAll} = require('@jest/globals');
 const mariadb = require('mariadb');
 const {registerNewUser} = require('../routes/user');
 const {getUserCoins} = require('../routes/coins');
@@ -11,32 +11,27 @@ const pool = mariadb.createPool({
     database: 'cargodb',
 });
 
-beforeAll(() => {
+async function getUserID(email) {
+    const conn = await pool.getConnection();
+    const idQuery = 'SELECT userId FROM user WHERE email = ?';
+    const result = await conn.query(idQuery, [email]);
+    return result[0].userId;
+}
+
+test('getUserCoins', async () => {
     const firstName = 'testFirstName';
     const lastName = 'testLastName';
     const email = 'mail@mail.de';
     const password = 'testPassword';
     const birthdate = '1990-01-01';
     const phonenumber = '1234567890';
-    registerNewUser(firstName, lastName, email, password, birthdate, phonenumber);
-});
+    await registerNewUser(firstName, lastName, email, password, birthdate, phonenumber);
 
-async function getUserID(email) {
-    const conn = await pool.getConnection();
-    const idQuery = 'SELECT userId FROM user WHERE email = ?';
-    const result = await conn.query(idQuery, [email]);
-    if (result[0].userId >= 0) {
-        return result[0].userId;
-    } else {
-        console.error(`Kein Benutzer mit der E-Mail-Adresse ${email} gefunden.`);
-        return null;
-    }
-}
-
-test('getUserCoins', async () => {
-    const id = await getUserID('mail@mail.de');
+    const id = await getUserID(email);
     const result = await getUserCoins(id);
+
     expect(result.data.coins).toBe(0);
+
     const conn = await pool.getConnection();
     const deleteQuery = 'DELETE FROM user WHERE userId = ?';
     await conn.query(deleteQuery, [id]);
