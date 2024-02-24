53347ad3b5f2a6acb7759b76e15027d484356d20
Benjamin Wirth <benjamin.wirth@mni.thm.de>
2024-02-24 12:43:57 +0100

    #184: Receive last messages in the frontend.

--- frontend/mycargonaut/src/app/chat/chat.component.ts
+++ frontend/mycargonaut/src/app/chat/chat.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Chatmessage} from "../chatmessage";
+import {ApiService} from "../service/api.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnInit {
+  adId: string | null = '4';
+  messageList: Chatmessage[] = [];
+
+  constructor(
+    private api: ApiService,
+    private route: ActivatedRoute,
+  ){}
+
+  ngOnInit(): void {
+    this.adId = this.route.snapshot.paramMap.get('id');
+    this.api.getRequest('chat/getLast/' + this.adId).subscribe((res: any) => {
+      this.messageList = res.data;
+    });
+  }
 
 }
