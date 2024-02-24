f7c3283ca19b2a263caa6aab539f6aa289785678
Benjamin Wirth <benjamin.wirth@mni.thm.de>
2024-02-24 12:51:44 +0100

    #184: First listing of messages in an ad.

--- frontend/mycargonaut/src/app/chat/chat.component.ts
+++ frontend/mycargonaut/src/app/chat/chat.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Chatmessage} from "../chatmessage";
 import {ApiService} from "../service/api.service";
 import {ActivatedRoute} from "@angular/router";
+import {AuthService} from "../service/auth.service";
 
 @Component({
   selector: 'app-chat',
@@ -11,13 +12,20 @@ import {ActivatedRoute} from "@angular/router";
 export class ChatComponent implements OnInit {
   adId: string | null = '4';
   messageList: Chatmessage[] = [];
+  ownUserId = -1;
 
   constructor(
     private api: ApiService,
     private route: ActivatedRoute,
+    private auth: AuthService,
   ){}
 
   ngOnInit(): void {
+    const authUserData = this.auth.getUserData();
+    if(authUserData != null) {
+      this.ownUserId = JSON.parse(authUserData).userId;
+    }
+
     this.adId = this.route.snapshot.paramMap.get('id');
     this.api.getRequest('chat/getLast/' + this.adId).subscribe((res: any) => {
       this.messageList = res.data;
