f607c790437145205b3bc03c12e057fb9d056c49
Marcel Kaiser <marcel.philipp.kaiser@mni.thm.de>
2024-02-27 11:42:12 +0100

    add backend tests for both routes

--- backend/test/coin.test.js
+++ backend/test/coin.test.js
@@ -2,6 +2,8 @@ const {expect, test, afterAll} = require('@jest/globals');
 const mariadb = require('mariadb');
 const {registerNewUser} = require('../routes/user');
 const {getUserCoins} = require('../routes/coins');
+const {addUserCoins} = require('../routes/coins');
+const {subtractUserCoins} = require('../routes/coins');
 
 const pool = mariadb.createPool({
     host: '0.0.0.0',
@@ -18,6 +20,9 @@ async function getUserID(email) {
     return result[0].userId;
 }
 
+
+
+
 test('getUserCoins', async () => {
     const firstName = 'testFirstName';
     const lastName = 'testLastName';
@@ -31,6 +36,53 @@ test('getUserCoins', async () => {
     const result = await getUserCoins(id);
 
     expect(result.data).toBe(0);
+
+    const conn = await pool.getConnection();
+    const deleteQuery = 'DELETE FROM user WHERE userId = ?';
+    await conn.query(deleteQuery, [id]);
+});
+
+test('addUserCoins', async () => {
+    const firstName = 'testFirstName';
+    const lastName = 'testLastName';
+    const email = 'mail@mail.de';
+    const password = 'testPassword';
+    const birthdate = '1990-01-01';
+    const phonenumber = '1234567890';
+    await registerNewUser(firstName, lastName, email, password, birthdate, phonenumber);
+
+    const id = await getUserID(email);
+    const coinsToAdd = 10;
+    await addUserCoins(id, coinsToAdd);
+
+    const result = await getUserCoins(id);
+
+    expect(result.data).toBe(coinsToAdd);
+
+    const conn = await pool.getConnection();
+    const deleteQuery = 'DELETE FROM user WHERE userId = ?';
+    await conn.query(deleteQuery, [id]);
+});
+
+test('subtractUserCoins', async () => {
+    const firstName = 'testFirstName';
+    const lastName = 'testLastName';
+    const email = 'mail@mail.de';
+    const password = 'testPassword';
+    const birthdate = '1990-01-01';
+    const phonenumber = '1234567890';
+    await registerNewUser(firstName, lastName, email, password, birthdate, phonenumber);
+
+    const id = await getUserID(email);
+    const coinsToAdd = 10;
+    await addUserCoins(id, coinsToAdd);
+
+    const coinsToSubtract = 5;
+    await subtractUserCoins(id, coinsToSubtract);
+
+    const result = await getUserCoins(id);
+
+    expect(result.data).toBe(coinsToAdd - coinsToSubtract);
 
     const conn = await pool.getConnection();
     const deleteQuery = 'DELETE FROM user WHERE userId = ?';
