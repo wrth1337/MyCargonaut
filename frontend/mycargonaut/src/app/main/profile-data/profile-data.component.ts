f71ae8ea0cf5172a0b350370f64ed2f4397c174d
Nico Binder <nicolas.daniel.binder@mni.thm.de>
2024-03-10 00:21:50 +0100

    delete comment and fix error

--- frontend/mycargonaut/src/app/main/profile-data/profile-data.component.ts
+++ frontend/mycargonaut/src/app/main/profile-data/profile-data.component.ts
@@ -56,9 +56,7 @@ export class ProfileDataComponent implements OnInit {
       });
     } else {
       this.userId = JSON.parse(this.auth.getUserData() || '{user_id = 0}').user_id;
-      //if(this.id == this.userId) {
-        this.isOwner = true;
-      //}
+      this.isOwner = true;
       this.getProfileData(this.userId);
     }
   }
