7a689dbac1395248803ba5a505750a7b62c862fc
Marcel Kaiser <marcel.philipp.kaiser@mni.thm.de>
2024-03-12 16:08:11 +0100

    refactor wanted.test.js

--- backend/test/wanted.test.js
+++ backend/test/wanted.test.js
@@ -17,22 +17,22 @@ test('get wanted by correct Id', async () =>{
         await conn.query(`INSERT INTO user (userId, firstName, lastName, email, password, birthdate, phonenumber, coins, picture, description, experience) \
                 VALUES (101337,'Max', 'Mustermann', 'max@example.com', 'pass123', '1990-05-15', '123456789', 100.0, 'user1.jpg', 'Hi was geht so', 'Viel Erfahrung')`);
         await conn.query(`INSERT INTO ad (adId,description, startLocation, endLocation, startDate, endDate, animals, smoker, notes, numSeats, userId) \
-                VALUES (101337,'Ja Beschreibung halt so lololol', 'City A', 'City B', '2023-01-10', '2023-01-15', 0, 1, 'No pets allowed', 4, 10)`);
+                VALUES (10,'Ja Beschreibung halt so lololol', 'City A', 'City B', '2023-01-10', '2023-01-15', 0, 1, 'No pets allowed', 4, 101337)`);
         await conn.query(`INSERT INTO wanted (wantedId, adId, freight) \
-                VALUES (101337, 10, 'Freight')`);
-        const res = (await wanted.getWantedById(101337));
+                VALUES (10, 10, 'Freight')`);
+        const res = (await wanted.getWantedById(10));
         expect(res.data).toEqual(
             {
-                'wantedId': 101337,
-                'adId': 101337,
+                'wantedId': 10,
+                'adId': 10,
                 'freight': 'Freight',
             });
     } finally {
         conn.query(`DELETE FROM ad WHERE adId = 10`);
         conn.query(`DELETE FROM wanted WHERE wantedId = 10`);
-        conn.query(`DELETE FROM user WHERE userId = 10`);
+        conn.query(`DELETE FROM user WHERE userId = 101337`);
         if (conn) await conn.release();
-    };
+    }
 });
 
 test('get wanted by wrong Id', async () =>{
