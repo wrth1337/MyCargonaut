d627d6407193f942fd04fd12a99e1e56ac5effd5
Marcel Kaiser <marcel.philipp.kaiser@mni.thm.de>
2024-03-12 16:51:20 +0100

    refactor add.test.js added await deffernt userID

--- backend/test/ad.test.js
+++ backend/test/ad.test.js
@@ -14,11 +14,11 @@ test('get ad by correct Id', async () => {
     try {
         conn = await pool.getConnection();
         await conn.query(`INSERT INTO user (userId, firstName, lastName, email, password, birthdate, phonenumber, coins, picture, description, experience) \
-                VALUES (10,'Max', 'Mustermann', 'max@example.com', 'pass123', '1990-05-15', '123456789', 100.0, 'user1.jpg', 'Hi was geht so', 'Viel Erfahrung')`);
+                VALUES (101338,'Max', 'Mustermann', 'max@example.com', 'pass123', '1990-05-15', '123456789', 100.0, 'user1.jpg', 'Hi was geht so', 'Viel Erfahrung')`);
         await conn.query(`INSERT INTO ad (adId,description, startLocation, endLocation, startDate, endDate, animals, smoker, notes, numSeats, userId) \
-                VALUES (10,'Ja Beschreibung halt so lololol', 'City A', 'City B', '2023-01-10', '2023-01-15', 0, 1, 'No pets allowed', 4, 10)`);
+                VALUES (10,'Ja Beschreibung halt so lololol', 'City A', 'City B', '2023-01-10', '2023-01-15', 0, 1, 'No pets allowed', 4, 101338)`);
         await conn.query(`INSERT INTO vehicle (vehicleId, name, numSeats, maxWeight, picture, loadingAreaDimensions, specialFeatures, userId) \
-                VALUES (10,'Car1', 4, 500.0, 'car1.jpg', '2x2x2', 'GPS, Bluetooth', 10)`);
+                VALUES (10,'Car1', 4, 500.0, 'car1.jpg', '2x2x2', 'GPS, Bluetooth', 101338)`);
         await conn.query(`INSERT INTO offer (offerId, vehicleId, adId, pricePerPerson, pricePerFreight) \
                 VALUES (10, 10, 10, 50.0, 100.0)`);
         const res = (await ad.getAdById(10));
@@ -36,15 +36,23 @@ test('get ad by correct Id', async () => {
                 'numSeats': 4,
                 'picture': null,
                 'state': 'created',
-                'userId': 10,
+                'userId': 101338,
             });
     } finally {
-        conn.query(`DELETE FROM ad WHERE adId = 10`);
-        conn.query(`DELETE FROM offer WHERE offerId = 10`);
-        conn.query(`DELETE FROM vehicle WHERE vehicleId = 10`);
-        conn.query(`DELETE FROM user WHERE userId = 10`);
+        await conn.query(`DELETE
+                          FROM ad
+                          WHERE adId = 10`);
+        await conn.query(`DELETE
+                          FROM offer
+                          WHERE offerId = 10`);
+        await conn.query(`DELETE
+                          FROM vehicle
+                          WHERE vehicleId = 10`);
+        await conn.query(`DELETE
+                          FROM user
+                          WHERE userId = 10`);
         if (conn) await conn.release();
-    };
+    }
 });
 
 test('get ad by wrong Id', async () => {
