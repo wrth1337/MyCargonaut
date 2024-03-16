4857c1ea4511bcc961b5b3eb2a05c27ca8f91e65
Marcel Kaiser <marcel.kaiser95@t-online.de>
2024-03-16 16:07:50 +0100

    #219 ad price to wanted test

--- backend/test/wanted.test.js
+++ backend/test/wanted.test.js
@@ -18,14 +18,15 @@ test('get wanted by correct Id', async () => {
         await conn.query(`INSERT INTO user (userId, firstName, lastName, email, password, birthdate, phonenumber, coins, picture, description, experience) VALUES (101337,'Max', 'Mustermann', 'max@example.com', 'pass123', '1990-05-15', '123456789', 100.0, 'user1.jpg', 'Hi was geht so', 'Viel Erfahrung')`);
         await conn.query(`INSERT INTO ad (adId,description, startLocation, endLocation, startDate, endDate, animals, smoker, notes, numSeats, userId) \
                 VALUES (10,'Ja Beschreibung halt so lololol', 'City A', 'City B', '2023-01-10', '2023-01-15', 0, 1, 'No pets allowed', 4, 101337)`);
-        await conn.query(`INSERT INTO wanted (wantedId, adId, freight) \
-                VALUES (10, 10, 'Freight')`);
+        await conn.query(`INSERT INTO wanted (wantedId, adId, freight, price) \
+                VALUES (10, 10, 'Freight', 420)`);
         const res = (await wanted.getWantedById(10));
         expect(res.data).toEqual(
             {
                 'wantedId': 10,
                 'adId': 10,
                 'freight': 'Freight',
+                'weight': 420
             });
     } finally {
         conn.query(`DELETE FROM ad WHERE adId = 10`);
