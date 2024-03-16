332f02ed80032b5080b69cfdc5a658cd07c5239d
Marcel Kaiser <marcel.kaiser95@t-online.de>
2024-03-16 16:19:44 +0100

    #219 fix id´s

--- backend/test/ad.test.js
+++ backend/test/ad.test.js
@@ -16,15 +16,15 @@ test('get ad by correct Id', async () => {
         // eslint-disable-next-line max-len
         await conn.query(`INSERT INTO user (userId, firstName, lastName, email, password, birthdate, phonenumber, coins, picture, description, experience) VALUES (101338,'Max', 'Mustermann', 'max@example.com', 'pass123', '1990-05-15', '123456789', 100.0, 'user1.jpg', 'Hi was geht so', 'Viel Erfahrung')`);
         await conn.query(`INSERT INTO ad (adId,description, startLocation, endLocation, startDate, endDate, animals, smoker, notes, numSeats, userId) \
-                VALUES (10,'Ja Beschreibung halt so lololol', 'City A', 'City B', '2023-01-10', '2023-01-15', 0, 1, 'No pets allowed', 4, 101338)`);
+                VALUES (1069,'Ja Beschreibung halt so lololol', 'City A', 'City B', '2023-01-10', '2023-01-15', 0, 1, 'No pets allowed', 4, 101338)`);
         await conn.query(`INSERT INTO vehicle (vehicleId, name, numSeats, maxWeight, picture, loadingAreaDimensions, specialFeatures, userId) \
-                VALUES (10,'Car1', 4, 500.0, 'car1.jpg', '2x2x2', 'GPS, Bluetooth', 101338)`);
+                VALUES (1020,'Car1', 4, 500.0, 'car1.jpg', '2x2x2', 'GPS, Bluetooth', 101338)`);
         await conn.query(`INSERT INTO offer (offerId, vehicleId, adId, pricePerPerson, pricePerFreight) \
-                VALUES (10, 10, 10, 50.0, 100.0)`);
-        const res = (await ad.getAdById(10));
+                VALUES (1030, 1020, 1069, 50.0, 100.0)`);
+        const res = (await ad.getAdById(1069));
         expect(res.data).toEqual(
             {
-                'adId': 10,
+                'adId': 1069,
                 'description': 'Ja Beschreibung halt so lololol',
                 'startLocation': 'City A',
                 'endLocation': 'City B',
@@ -41,13 +41,13 @@ test('get ad by correct Id', async () => {
     } finally {
         await conn.query(`DELETE
                           FROM ad
-                          WHERE adId = 10`);
+                          WHERE adId = 1069`);
         await conn.query(`DELETE
                           FROM offer
-                          WHERE offerId = 10`);
+                          WHERE offerId = 1030`);
         await conn.query(`DELETE
                           FROM vehicle
-                          WHERE vehicleId = 10`);
+                          WHERE vehicleId = 1020`);
         await conn.query(`DELETE
                           FROM user
                           WHERE userId = 10`);
